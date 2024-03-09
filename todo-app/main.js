98fbb14c65ed123ef763b147cb2d0e5e3bc3af97
Anna Hilla <annahilla21@gmail.com>
2024-03-09 14:18:27 +0100

    Changed UI

--- todo-app/main.js
+++ todo-app/main.js
@@ -14,21 +14,30 @@ let emptyWork = document.getElementById('emptyWork');
 let emptyPersonal = document.getElementById('emptyPersonal');
 let emptyErrands = document.getElementById('emptyErrands');
 let empties = document.querySelectorAll('.empty-task-item');
+let personalTaskInput = document.getElementById('personalTaskInput');
+let workTaskInput = document.getElementById('workTaskInput');
+let errandsTaskInput = document.getElementById('errandsTaskInput');
+let workTask = document.getElementById('workTask');
+let personalTask = document.getElementById('personalTask');
+let errandsTask = document.getElementById('errandsTask');
 let draggedTask;
 
 // Event Listeners
-taskInput.addEventListener('keypress', addTask);
+workTaskInput.addEventListener('keypress', addTask);
+personalTaskInput.addEventListener('keypress', addTask);
+errandsTaskInput.addEventListener('keypress', addTask);
 
-// Task function
+
 function addTask(event) {
+    let taskInputValue = event.target.value;
     if (event.key === "Enter") {
         let task = document.createElement('div');
         task.classList.add('task-item');
         task.setAttribute('draggable', 'true');
         task.innerHTML = `
             <div class="task-value">
-            <p>${taskInput.value}<p>
-            <small class="priority"></small>
+            <p>${taskInputValue}<p>
+            <small class="priority mediumPriorityChecked">Medium Priority</small>
             <i id="remove" class="remove-btn fa-solid fa-xmark"></i>
             </div>
         `;
@@ -37,31 +46,22 @@ function addTask(event) {
         let removeBtn = task.querySelector('.remove-btn');
         removeBtn.addEventListener('click', () => task.remove());
 
-        if (workCategory.checked) {
+        let category = event.target.parentElement.parentElement.id;
+        console.log(category);
+
+        if (category === 'workList') {
             workList.insertBefore(task, emptyWork);
 
-        } else if (personalCategory.checked) {
+        } else if (category === 'personalList') {
             personalList.insertBefore(task, emptyPersonal);
 
-        } else if (errandsCategory.checked) {
+        } else if (category === 'errandsList') {
             errandsList.insertBefore(task, emptyErrands);
         }
 
-        //Priorities
-        if (highPriority.checked) {
-            priorityTag.innerHTML = "High Priority";
-            priorityTag.style.backgroundColor = 'red';
-            priorityTag.classList.add('highPriorityChecked');
-        } else if (mediumPriority.checked) {
-            priorityTag.innerHTML = "Medium Priority";
-            priorityTag.classList.add('mediumPriorityChecked');
-        } else if (lowPriority.checked) {
-            priorityTag.innerHTML = "Low Priority";
-            priorityTag.style.backgroundColor = 'green';
-            priorityTag.classList.add('lowPriorityChecked');
-        }
-
-        taskInput.value = '';
+        workTaskInput.value = '';
+        personalTaskInput.value = '';
+        errandsTaskInput.value = '';
         taskItem = task;
 
         //Priority Config
